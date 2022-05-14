35085c6c5d3b1d03e2e9f6bf82c386f105042fda
omotega <tomoyibo@gmail.com>
2022-05-14 22:44:42 +0100

    used async handler

--- backend/controllers/postcontroller.js
+++ backend/controllers/postcontroller.js
@@ -1,62 +1,44 @@
 const Post = require("../models/postmodel");
 const asynchandler = require('express-async-handler')
-const getposts = async (req, res) => {
-  try {
-    const post = await Post.find();
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(400);
-    throw error;
-  }
-};
 
-const createpost = async (req, res) => {
-  try {
-    if (!req.body.text) {
-      res.status(400);
-      throw new Error("please input the text field");
-    }
-    const post = await Post.create({
-      text: req.body.text,
-    });
-    res.status(200).json(post)
-  } catch (error) {
-    res.status(400);
-    throw new Error('could not create post');
-  }
-};
+const getposts = asynchandler(async (req, res) => {
+  const post = await Post.find();
+  res.status(200).json(post);
+});
 
-const updatepost = async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id)
-    if (!post) {
-      res.status(400)
-      throw new Error('post does not exist')
-    }
-    const updatedpost = await Post.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    })
-    res.status(200).json(updatedpost);
-  } catch (error) {
+const createpost = asynchandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("please input the text field");
+  }
+  const post = await Post.create({
+    text: req.body.text,
+  });
+  res.status(200).json(post)
+});
+
+const updatepost = asynchandler(async (req, res) => {
+  const post = await Post.findById(req.params.id)
+  if (!post) {
     res.status(400)
-    throw new Error('could not update post')
+    throw new Error('post does not exist')
   }
-};
+  const updatedpost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+  res.status(200).json(updatedpost);
 
-const deletepost = async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id)
-    if (!post) {
-      res.status(400)
-      throw new Error('post does not exist')
-    }
-    await Post.deleteOne(post);
-    res.status(200).json({ id: req.params.id });
-  } catch (error) {
-    res.status(400);
-    throw new Error('user not deleted');
+});
+
+const deletepost = asynchandler(async (req, res) => {
+  const post = await Post.findById(req.params.id)
+  if (!post) {
+    res.status(400)
+    throw new Error('post does not exist')
   }
-};
+  await Post.deleteOne(post);
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = {
   getposts,
--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,16 +1,16 @@
 const User = require("../models/usermodel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const asynchandler = require('express-async-handler');
 
-const register = async (req, res) => {
-  try {
+const register = asynchandler(async (req, res) => {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
       res.status(400);
       throw new Error("please fill in the required field");
     }
 
-    const userexist = await User.findOne({ email });
+    const userexist = await User.findOne({email});
     if (userexist) {
       res.status(400);
       throw new Error("user already exist");
@@ -34,13 +34,21 @@ const register = async (req, res) => {
       res.status(400);
       throw new Error("invalid credentials");
     }
-  } catch (error) {
-    res.status(400);
-    throw new Error("could not create user");
-  }
-};
+  
+});
 
 const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({email});
+        if(!user) {
+            res.status(400)
+            throw new Error('user does not exist')
+        }
+
+    } catch (error) {
+        
+    }
   res.status(200).json({ message: login });
 };
 
